a58b3a24c9369fa59c35edaf4504f98ff62db6ae
Bien Cabungcal <biencabungcal.dev@gmail.com>
2025-08-24 04:36:14 +0800

    Base Project with base interaction

---
+++ Prototype/Source/Prototype/Private/PrototypeCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Public/PrototypeCameraManager.h"
+
+APrototypeCameraManager::APrototypeCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
---
+++ Prototype/Source/Prototype/Private/PrototypeGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Public/PrototypeGameMode.h"
+
+APrototypeGameMode::APrototypeGameMode()
+{
+	// stub
+}
